1cc6e2ed9e865486df1a4b0ffa37078334538cce
Gonzsou <42410932+Gonzsou@users.noreply.github.com>
2019-07-28 23:17:58 +0100

    Add files via upload
    Arrays are always passed to functions by reference. Exemplification

---
+++ array_to_function.c
@@ -0,0 +1,38 @@
+/*
+ * C - Basics
+ * Passing array to a function (exemplification as arrays are always passed by reference)
+ * (array_to_function.c) v20190728
+ *
+ * Gonzsou
+ *
+ */
+ 
+ #include<stdio.h>
+ #include<stdlib.h>
+ 
+void foo(int *parametro, int n){
+	int i;
+	for (i=0;i<n;i++){
+		printf("%d", parametro[i]++);		//prints elements of array passed to function and increments each of its elements right afterwards
+	}
+} 
+
+int main(){
+
+	int i, array[] = {3,5,8,6,8};				
+	int n = sizeof(array)/sizeof(int);	//calculates number of elements of initialized integer array 
+
+	/*Values before function call*/
+	printf("Array before function call:\t");
+	foo(array, n);
+	printf("\n");
+
+	/*Values after function call*/
+	printf("Array after function call:\t");
+	for (i=0;i<n;i++){
+			printf("%d", array[i]);
+	}
+	printf("\n");
+	
+	return (0);
+} 
