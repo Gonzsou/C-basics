a0d7f3c5c3021dcda6a8cc34ab38183aed2dd7cb
Gonzsou <42410932+Gonzsou@users.noreply.github.com>
2020-05-24 01:10:28 +0100

    Update numeros_irmaos.c

--- numeros_irmaos.c
+++ numeros_irmaos.c
@@ -7,56 +7,44 @@ void NumerosIrmaos(){
   	scanf("%d %d", &num1, &num2);
 
 	int i, j, isPrime, fator1=1, fator2=0; 
-	for (i = 2; i <= num1; i++)
-	{
-		if(num1 % i == 0)
-		{
+	for (i = 2; i <= num1; i++) {
+		if(num1 % i == 0) {
 			isPrime = 1;
-			for (j = 2; j <= i/2; j++)
-			{
-				if(i % j == 0)
-				{
+			for (j = 2; j <= i/2; j++) {
+				if(i % j == 0) {
 					isPrime = 0;
 					break;
 				}
 			} 
-			if(isPrime == 1)
-			{
+			if(isPrime == 1) {
 				fator1=i;
 			}         	
 		}
 	}
-	for (i = 2; i <= num2; i++)
-	{
-		if(num2 % i == 0)
-		{
+	for (i = 2; i <= num2; i++) {
+		if(num2 % i == 0) {
 			isPrime = 1;
-			for (j = 2; j <= i/2; j++)
-			{
-				if(i % j == 0)
-				{
+			for (j = 2; j <= i/2; j++) {
+				if(i % j == 0) {
 					isPrime = 0;
 					break;
 				}
 			} 
-			if(isPrime == 1)
-			{
+			if(isPrime == 1) {
 				fator2=i;
 			}         	
 		}
 	}
 //	printf("\n fator1: %d ", fator1);
 //	printf("\n fator2: %d ", fator2);
-	if(fator1!=fator2){
+	if(fator1!=fator2) {
 		printf("\n%d e %d não sao numeros irmãos.\n", num1, num2);
 	} else {
 		printf("\n%d e %d sao numeros irmãos.\n", num1, num2);
 	}
 }
 
-int main(void){
-	
+int main(void) {
 	NumerosIrmaos();
-	
 	return 0;
 }
